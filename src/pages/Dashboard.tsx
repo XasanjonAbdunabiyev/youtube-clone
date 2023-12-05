32e2341ecdc3551ccc43ae98eac9512e326fe517
xasanjon <xasanjonabdunabiyev222@gmail.com>
2023-12-05 12:35:18 +0500

    vercel error fixed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,6 @@ import { Layout } from "@/layouts/Layout";
 import { Heading } from "@chakra-ui/react";
 
 export default function Dashboard() {
-
     return (
         <Layout showCategries={false}>
             <Heading as="h3" size="30px">Контент канала</Heading>
